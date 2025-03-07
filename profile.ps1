db2d9d8715522a6973ee689c1c47961c8f0d8dc6
Piotr Kramarczy <github.piotr@proton.me>
2025-03-07 15:11:21 +0100

    upd

--- profile.ps1
+++ profile.ps1
@@ -21,11 +21,26 @@ function mcc {
 
 
 # searches for directories, previews and navigates to the selected folder
-function cdd {
+function cdd1 {
     $dir = Get-ChildItem -Path $HOME -Directory -Recurse -Force | ForEach-Object { $_.FullName } | 
     $dirs = $dirs + $HOME  # Katalog domowy zawsze na końcu listy
     $dir = $dirs
     fzf --exact --prompt="Enter directory template: " --preview='dir {}'
+
+    if ($dir) {
+        cls
+        Set-Location -Path $dir
+        Get-ChildItem -Force
+    }
+}
+
+
+
+function cdd {
+    $dirs = Get-ChildItem -Path $HOME -Directory -Recurse -Force | ForEach-Object { $_.FullName }
+    $dirs += $HOME  # Dodaj katalog domowy na końcu listy
+
+    $dir = $dirs | fzf --exact --prompt="Enter directory template: " --preview='dir {}'
 
     if ($dir) {
         cls
