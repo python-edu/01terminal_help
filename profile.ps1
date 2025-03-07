68c0de7f4e4be16921cf3d41e2571c42a8790e9e
Piotr Kramarczy <github.piotr@proton.me>
2025-03-07 11:58:05 +0100

    profile

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,5 @@
 $env:EDITOR = "nvim"
+
 if (-not $env:EDITOR) {
     $env:EDITOR = "micro"
 }
@@ -6,22 +7,26 @@ if (-not $env:EDITOR) {
 Set-Alias mc $env:EDITOR
 
 
+# Funkcja mcc - wyszukiwanie plików i otwieranie w edytorze
 function mcc {
-    $file = Get-ChildItem -Path $HOME -File -Recurse -Force | ForEach-Object { $_.FullName } | fzf --exact --prompt="Enter file pattern: " --info=inline `
-        --preview='if (Test-Path {} -PathType Leaf) { Get-Content -Path {} -ErrorAction SilentlyContinue } else { " ---" }'
+    $file = Get-ChildItem -Path $HOME -File -Recurse -Force | ForEach-Object { $_.FullName } | 
+        fzf --exact --prompt="Enter file pattern: " --info=inline `
+            --preview='if (Test-Path {} -PathType Leaf) { Get-Content -Path {} -ErrorAction SilentlyContinue | Out-String } else { " ---" }'
 
     if ($file) {
-        & $global:EDITOR $file
+        & $env:EDITOR $file
     }
 }
 
+# Funkcja cdd - wyszukiwanie katalogów i przechodzenie do wybranego
 function cdd {
-    $dir = Get-ChildItem -Path $HOME -Directory -Recurse -Force | ForEach-Object { $_.FullName } | fzf --exact --prompt="Enter directory template: " `
-        --preview='tree -L 1 -F --dirsfirst {}'
+    $dir = Get-ChildItem -Path $HOME -Directory -Recurse -Force | ForEach-Object { $_.FullName } | 
+        fzf --exact --prompt="Enter directory template: " `
+            --preview='dir {}'
 
     if ($dir) {
+        cls
         Set-Location -Path $dir
         Get-ChildItem -Force
     }
 }
-
