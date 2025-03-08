e33e612e93646c62e33d13854751acaae0a2880d
Piotr Kramarczy <github.piotr@proton.me>
2025-03-08 16:35:31 +0100

    opis w bash_func.sh

--- bash_func.sh
+++ bash_func.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+#
+# The script contains functions that help search files and directories on the disk in the user's home directory. Each
+# function exists in two versions:
+#   - the first version uses the default programs in Linux: find and cat
+#   - the second version uses programs that must be installed by the user: `fd` - a replacement for `find` and `bat`
+#     (batcat) - a replacement for `cat`
+#
+# All functions use the `fzf` program, which must be installed by the user.
+#
+# Functions:
+# - mcc: searches for files, displays the contents of text files and opens the selected file for reading
+# - cdd: searches for directories, displays the contents of the selected directory and changes to the selected directory
+
 
 # edit the line below - enter your editor
 EDITOR="nvim"  #  "micro", "vim", "nano", ...
 
 alias mc=$EDITOR
+complete -o default -o filenames mc
+
 
 # Allows the user to set their favorite editor (default is nano)
 export EDITOR="${EDITOR:-micro}"
@@ -15,6 +30,21 @@ mcc() {
         | fzf --exact \
               --prompt="Enter file pattern: " \
               --info=inline \
+              --preview='if file --mime {} | grep -q text; then cat {}; else echo " ---"; fi'
+         )
+    
+    if [[ -n "$file" ]]; then
+        "$EDITOR" "$file"
+    fi
+}
+
+
+mcc() {
+    local file
+    file=$(fd . "$HOME" -u -a -t f -I -E .git \
+        | fzf --exact \
+              --prompt="Enter file pattern: " \
+              --info=inline \
               --preview='if file --mime {} | grep -q text; then batcat --color=always {} --style=numbers,changes; else echo " ---"; fi'
          )
     
@@ -22,6 +52,8 @@ mcc() {
         "$EDITOR" "$file"
     fi
 }
+
+
 
 # cdd function - search directories and go to the selected one
 cdd() {
@@ -40,7 +72,17 @@ cdd() {
 }
 
 
-# Export functions to work in terminal
-export -f mcc
-export -f cdd
-
+cdd() {
+    local dir
+    dir=$(fd . "$HOME" -u -a -t d -I -E .git \
+        | fzf --exact \
+              --prompt="Enter directory template: " \
+              --preview='tree -L 1 -F --dirsfirst {}'
+          )
+    
+    if [[ -n "$dir" ]]; then
+        clear
+        cd "$dir"
+        ls -gh
+    fi
+}
