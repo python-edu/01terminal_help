7f86c833936fec7e6c1b4eaccb7d41930d0d72d0
Piotr Kramarczy <github.piotr@proton.me>
2025-03-08 16:51:10 +0100

    update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,22 @@
+# This file contains the configuration for PowerShell:
+#   - download the file to disk
+#   - cp prfile.sc1 $PROFILE - copy the file to the default path $PROFILE
+#   - edit the file and set the $env:EDITOR variable
+#
+# The script contains functions that help search files and directories on the disk in the user's home directory.
+# The functions use additional programs that the user must install using e.g. the `scoop` manager. Additional programs:
+#   - micro - text editor
+#   - fd - disk search program (it is the input to fzf)
+#   - fzf - general-purpose command-line fuzzy finder
+#   - bat - displays the contents of a file in the terminal
+# 
+# Functions:
+#   - mcc: searches for files, displays the contents of text files and opens the selected file for reading
+#   - cdd: searches for directories, displays the contents of the selected directory and goes to the selected directory
+
+
 # edit the line below - enter your editor
-$env:EDITOR = "nvim"  #  "micro", "vim", "code", ...
+$env:EDITOR = "micro"  #  "micro", "vim", "code", ...
 
 # Alias
 Set-Alias mc $env:EDITOR
@@ -34,16 +51,3 @@ function cdd {
     }
 }
 
-
-function cdd-copy {
-    $dirs = Get-ChildItem -Path $HOME -Directory -Recurse -Force | ForEach-Object { $_.FullName }
-    $dirs += $HOME  # Dodaj katalog domowy na końcu listy
-
-    $dir = $dirs | fzf --exact --prompt="Enter directory template: " --preview='dir {}'
-
-    if ($dir) {
-        cls
-        Set-Location -Path $dir
-        Get-ChildItem -Force
-    }
-}
